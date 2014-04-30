99bee1dfaca1badd52f24ec0df6a4df176f19abf
tr3quart1sta <kyriakos.schwarz@students.fhnw.ch>
2014-05-01 01:20:14 +0200

    shorter version

--- kryg/Euler182.py
+++ kryg/Euler182.py
@@ -6,28 +6,13 @@
 from fractions import gcd
 
 p = 1009
-
 q = 3643
-
-n = p * q
-
 fin = (p-1)*(q-1)
-
-# possible e's
-poses = [elem for elem in range(3,fin) if gcd(fin, elem) == 1]  
-
-min = n
-
+min = 9
 sum = 0
 
-for e in poses:
-	x = (1+gcd(e-1,p-1))*(1+gcd(e-1,q-1))
-	if x < min:
-		min = x
-
-for e in poses:
-	x = (1+gcd(e-1,p-1))*(1+gcd(e-1,q-1))
-	if x == min:
+for e in range(3, fin):
+	if ( gcd(fin, e) == 1 ) and ( (1+gcd(e-1,p-1)) * (1+gcd(e-1,q-1)) == min ):
 		sum += e
 		
 print sum
